3fee94eaa804edb734ad19f6de7ae56b2e0464c7
FirstSS-Sub <sota.monokuma.bass.1@gmail.com>
2022-05-20 08:28:16 +0900

    update

--- leetcode/roman-to-integer.py
+++ leetcode/roman-to-integer.py
@@ -1,29 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        s += "0"
+        dic = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
         ans_count = 0
-        for i in range(len(s) - 1):
-            if s[i] == "I":
-                if s[i + 1] == "V" or s[i + 1] == "X":
-                    ans_count -= 1
-                else:
-                    ans_count += 1
-            elif s[i] == "V":
-                ans_count += 1
-            elif s[i] == "X":
-                if s[i + 1] == "L" or s[i + 1] == "C":
-                    ans_count -= 10
-                else:
-                    ans_count += 10
-            elif s[i] == "L":
-                ans_count += 50
-            elif s[i] == "C":
-                if s[i + 1] == "D" or s[i + 1] == "M":
-                    ans_count -= 100
-                else:
-                    ans_count += 100
-            elif s[i] == "D":
-                ans_count += 500
-            elif s[i] == "M":
-                ans_count += 1000
+        if not s:
+            return 0
+        ans_count += dic[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
+            if dic[s[i]] >= dic[s[i+1]]:
+                ans_count += dic[s[i]]
+            else:
+                ans_count -= dic[s[i]]
         return ans_count
