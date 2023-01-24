0a5b78648ec799ece077c99a2b9312d364179d63
FirstSS-Sub <sota.monokuma.bass.1@gmail.com>
2023-01-24 19:21:15 +0900

    leetcode/rust formated

--- leetcode/rust/src/middle_of_the_linked_list.rs
+++ leetcode/rust/src/middle_of_the_linked_list.rs
@@ -1,21 +1,16 @@
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
 }
 impl Solution {
-    pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        
-    }
+    pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {}
 }
