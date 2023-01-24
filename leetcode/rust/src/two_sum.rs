92eab719b3f630cefaf712986f9378801fe73808
FirstSS-Sub <sota.monokuma.bass.1@gmail.com>
2023-01-24 22:35:39 +0900

    leetcode/rust two_sum improved

--- leetcode/rust/src/two_sum.rs
+++ leetcode/rust/src/two_sum.rs
@@ -1,11 +1,27 @@
+use std::collections::HashMap;
+
+// impl Solution {
+//     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+//         for i in 0..nums.len() {
+//             for j in (i + 1)..nums.len() {
+//                 if (nums[i] + nums[j]) == target {
+//                     return vec![i as i32, j as i32];
+//                 }
+//             }
+//         }
+
+//         vec![]
+//     }
+// }
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for i in 0..nums.len() {
-            for j in (i + 1)..nums.len() {
-                if (nums[i] + nums[j]) == target {
-                    return vec![i as i32, j as i32];
-                }
+        let mut map = HashMap::new();
+        for (i, num) in nums.iter().enumerate() {
+            if let Some(&j) = map.get(&(target - num)) {
+                return vec![i as i32, j];
             }
+            map.insert(num, i as i32);
         }
 
         vec![]
